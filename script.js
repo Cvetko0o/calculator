c64883de558aad35ae0446d3734b1d3b637f76b1
Cvetko0o <55314876+Cvetko0o@users.noreply.github.com>
2022-03-20 20:51:46 +0200

    change buttons

--- script.js
+++ script.js
@@ -1,87 +1,33 @@
-let input = document.getElementsByClassName('input');
-let multiply = document.getElementById('*');
-let divide = document.getElementById('/');
-let add = document.getElementById('plus');
-let subtract = document.getElementById('minus');
-let number = document.getElementsByClassName('numbers');
-let one = document.getElementById('1');
-let two = document.getElementById('2');
-let three = document.getElementById('3');
-let four = document.getElementById('4');
-let five = document.getElementById('5');
-let six = document.getElementById('6');
-let seven = document.getElementById('7');
-let eight = document.getElementById('8');
-let nine = document.getElementById('9');
-let zero = document.getElementById('0');
+let input = document.getElementById('input');
+let numbers = document.querySelectorAll('.numbers button');
+let operators = document.querySelectorAll('.operators button');
 let point = document.getElementById('point');
 let clear = document.getElementById('clear');
 let result = document.getElementById('result');
+let container;
 
-
-add.addEventListener('click', function () {
-    input[0].textContent += '+';
+numbers.forEach( number => { 
+    number.addEventListener('click', function(){
+        
+        if(number.textContent == 'C'){
+            
+        } else {
+            input.textContent += number.textContent;
+        }
+        container += input.textContent;
+    });
 });
 
-subtract.addEventListener('click', function () {
-    input[0].textContent += '-';
-});
-
-multiply.addEventListener('click', function () {
-    input[0].textContent += '*';
-});
-
-divide.addEventListener('click', function () {
-    input[0].textContent += '/';
-});
-
-one.addEventListener('click', function(){
-    input[0].textContent += Number(this.id);
-});
-
-two.addEventListener('click', function(){
-    input[0].textContent += Number(this.id);
-});
-
-three.addEventListener('click', function(){
-    input[0].textContent += Number(this.id);
-});
-
-four.addEventListener('click', function(){
-    input[0].textContent += Number(this.id);
-});
-
-five.addEventListener('click', function(){
-    input[0].textContent += Number(this.id);
-});
-
-six.addEventListener('click', function(){
-    input[0].textContent += Number(this.id);
-});
-
-seven.addEventListener('click', function(){
-    input[0].textContent += Number(this.id);
-});
-
-eight.addEventListener('click', function(){
-    input[0].textContent += Number(this.id);
-});
-
-nine.addEventListener('click', function(){
-    input[0].textContent += Number(this.id);
-});
-
-zero.addEventListener('click', function(){
-    input[0].textContent += Number(this.id);
-});
-
-point.addEventListener('click', function(){
-    input[0].textContent += '.';
+operators.forEach(operator => {
+    operator.addEventListener('click', function(){
+        input.textContent += operator.textContent;
+        container += input.textContent;
+    });
 });
 
 clear.addEventListener('click', function(){
-    input[0].textContent = input[0].textContent.slice(0, -1);
-});
+    input.textContent = input.textContent.slice(0, -1);
+ });
 
 result.addEventListener('click', function(){
 
